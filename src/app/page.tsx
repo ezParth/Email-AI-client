39b9cde0d2d62c9c5ab9823546039646948caf4b
ezParth <parthsingh55m@gmail.com>
2025-04-12 12:10:55 +0530

    prisma and clerk setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "~/components/ui/button";
+
 export default async function Home() {
   return(
-    <h1 className="text-red-500 bg-black">Hello World</h1>
+    <div className="bg-white h-screen w-screen">
+      <Button className="">Press me daddy!</Button>
+    </div>
   )
 }
