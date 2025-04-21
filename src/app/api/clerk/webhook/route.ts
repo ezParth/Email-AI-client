83d6fd8c403a91af6c12ddb166839f9fc29f132f
ezParth <parthsingh55m@gmail.com>
2025-04-21 16:50:03 +0530

    fixed webhook

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -1,10 +1,35 @@
+import { db } from "~/server/db";
+
 export const POST = async (req: Request) => {
   try {
-    const { data } = await req.json();
-    console.log("Webhook from clerk: ", data);
+    const { data, type } = await req.json();
 
-    return new Response("Webhook recieved", { status: 200 });
+    if (type !== "user.created") {
+      return new Response("Event ignored", { status: 200 });
+    }
+
+    const emailAddress = data.email_addresses?.[0]?.email_address ?? "";
+    const firstName = data.first_name ?? "";
+    const lastName = data.last_name ?? "";
+    const imageUrl = data.image_url ?? "";
+    const id = data.id;
+
+    await db.user.upsert({
+      where: { id },
+      update: {},
+      create: {
+        id,
+        email: emailAddress,
+        firstName,
+        lastName,
+        imageUrl
+      }
+    });
+
+    console.log("User Created!");
+    return new Response("Webhook received", { status: 200 });
   } catch (error) {
     console.log("ERROR IN POST Request: ", error);
+    return new Response("Internal Server Error", { status: 500 });
   }
 };
